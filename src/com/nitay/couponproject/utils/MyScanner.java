9c6b36aab5456403603462a5cf026deb5db9042d
Nitay Caspi <NitayCaspi12@gmail.com>
2022-03-04 09:36:52 +0200

    create getCategory method

--- src/com/nitay/couponproject/utils/MyScanner.java
+++ src/com/nitay/couponproject/utils/MyScanner.java
@@ -1,5 +1,6 @@
 package com.nitay.couponproject.utils;
 
+import com.nitay.couponproject.model.Category;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -33,8 +34,16 @@ public class MyScanner {
             System.out.println(message);
             return new Scanner(System.in).nextDouble();
         } catch (InputMismatchException e) {
-            System.out.println("Wrong input please try again");
-            return getDoubleInput(message);
+            return getDoubleInput("Wrong input please try again: ");
+        }
+    }
+    public static Category getCategory(String message){
+        try{
+            System.out.println(message);
+            return Category.valueOf(new Scanner(System.in).next());
+        }
+        catch (InputMismatchException e){
+            return getCategory("Wrong input please try again: ");
         }
     }
 }
