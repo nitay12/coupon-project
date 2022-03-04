cead4d897067582093b79cff43749ac7f516367d
Nitay Caspi <NitayCaspi12@gmail.com>
2022-03-04 09:11:31 +0200

    add javadoc

--- src/com/nitay/couponproject/dal/interfaces/CouponsDAO.java
+++ src/com/nitay/couponproject/dal/interfaces/CouponsDAO.java
@@ -1,27 +1,160 @@
 package com.nitay.couponproject.dal.interfaces;
 
+import com.nitay.couponproject.exceptions.CrudException;
 import com.nitay.couponproject.model.Category;
 import com.nitay.couponproject.model.Coupon;
 
 import java.util.ArrayList;
 
+/**
+ * An interface for coupons data access object
+ */
 public interface CouponsDAO {
-    long addCoupon(Coupon coupon);
-    void updateCoupon(Coupon coupon);
-    void deleteCoupon(int couponID);
+    /**
+     * Adds a new company to the database
+     *
+     * @param coupon A Coupon object (no Id required)
+     * @return Auto generated coupon ID
+     * @throws CrudException if something gets wrong.
+     * @see Coupon
+     * @see CrudException
+     */
+    long addCoupon(Coupon coupon) throws CrudException;
 
-    void deleteCompanyCoupons(int companyID);
+    /**
+     * Updates an existing coupon in the database
+     *
+     * @param coupon A full Coupon object (Id param is required)
+     * @throws CrudException if something gets wrong.
+     * @see Coupon
+     * @see CrudException
+     */
+    void updateCoupon(Coupon coupon) throws CrudException;
 
-    ArrayList<Coupon> getAllCoupons();
-    Coupon getOneCoupon(int couponID);
-    long addCouponPurchase(long customerId, int couponId);
+    /**
+     * Deletes a coupon from the database
+     *
+     * @param couponID The coupon id in the database
+     * @throws CrudException if something gets wrong.
+     * @see CrudException
+     */
+    void deleteCoupon(int couponID) throws CrudException;
 
-    //Separated this method into two methods for better implementation
-    //void deleteCouponPurchase(int customerId, int couponId);
+    /**
+     * Deletes all company's coupons
+     *
+     * @param companyID The company id
+     * @throws CrudException if something gets wrong.
+     * @see CrudException
+     */
+    void deleteCompanyCoupons(int companyID) throws CrudException;
 
-    void deleteCouponPurchase(int couponId);
+    /**
+     * Gets all coupons that exist in the database
+     *
+     * @return An ArrayList of all coupons
+     * @throws CrudException if something gets wrong.
+     * @see Coupon
+     * @see CrudException
+     */
+    ArrayList<Coupon> getAllCoupons() throws CrudException;
 
-    void deleteCustomerPurchase(long customerId);
+    /**
+     * Gets a coupon from the database by given ID
+     *
+     * @param couponID The coupon id in the database
+     * @return A Coupon object
+     * @throws CrudException if something gets wrong
+     * @see Coupon
+     * @see CrudException
+     */
+    Coupon getOneCoupon(int couponID) throws CrudException;
 
-    int getCategoryId(Category category);
+    /**
+     * Add a one coupon purchase by one customer
+     *
+     * @param customerId The customer id (must exist in the database)
+     * @param couponId   The coupon id (must exist in the database)
+     * @return The Coupon id
+     * @throws CrudException if something gets wrong
+     * @see CrudException
+     */
+    long addCouponPurchase(long customerId, int couponId) throws CrudException;
+
+    //I separated the delete purchase method into two methods for better implementation
+
+
+    /**
+     * Deletes a coupon purchase
+     *
+     * @param couponId The coupon id
+     * @throws CrudException if something gets wrong.
+     */
+    void deletePurchaseByCouponId(long couponId) throws CrudException;
+
+    /**
+     * Deletes a coupon purchase
+     *
+     * @param customerId The customer id
+     * @throws CrudException if something gets wrong
+     */
+
+    void deletePurchaseByCustomerId(long customerId) throws CrudException;
+
+    /**
+     * Gets all the given company's coupons
+     *
+     * @param companyId The company id in the database
+     * @return ArrayList of all company's coupons
+     */
+    ArrayList<Coupon> getCompanyCoupons(long companyId) throws CrudException;
+
+    /**
+     * Gets all the given company's coupons filtered by given category
+     *
+     * @param companyId The company id in the database
+     * @param category  A Category (enum) refers the category column in the database
+     * @return ArrayList of all company's coupons filtered by category
+     */
+
+    ArrayList<Coupon> getCompanyCoupons(long companyId, Category category) throws CrudException;
+
+    /**
+     * Gets all coupons of the given company up to the given maximum price
+     *
+     * @param companyId The company id in the database
+     * @param maxPrice  Maximum price to filter the result
+     * @return ArrayList of all company's coupons up to maxPrice
+     * @throws CrudException if something gets wrong
+     */
+    ArrayList<Coupon> getCompanyCoupons(long companyId, double maxPrice) throws CrudException;
+
+    /**
+     * Gets all given customer's coupons
+     *
+     * @param customerId The customer id in the database
+     * @return An ArrayList of all given customer's coupons
+     * @throws CrudException if something gets wrong.
+     * @see Coupon
+     * @see CrudException
+     */
+    ArrayList<Coupon> getCustomerCoupons(long customerId) throws CrudException;
+
+    /**
+     * Gets all the given customer's coupons filtered by given category
+     *
+     * @param customerId The customer id in the database
+     * @param category  A Category (enum) refers the category column in the database
+     * @return ArrayList of all customer's coupons filtered by category
+     */
+    ArrayList<Coupon> getCustomerCoupons(long customerId, Category category) throws CrudException;
+    /**
+     * Gets all coupons of the given customer up to the given maximum price
+     *
+     * @param customerId The customer id in the database
+     * @param maxPrice  Maximum price to filter the result
+     * @return ArrayList of all customer's coupons up to maxPrice
+     * @throws CrudException if something gets wrong.
+     */
+    ArrayList<Coupon> getCustomerCoupons(long customerId, double maxPrice) throws CrudException;
 }
