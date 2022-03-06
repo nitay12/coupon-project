62b2fb371138968440ee62a9a2496d68c15943cf
Nitay Caspi <NitayCaspi12@gmail.com>
2022-03-06 10:22:09 +0200

    reformat printed message

--- src/com/nitay/couponproject/utils/MyScanner.java
+++ src/com/nitay/couponproject/utils/MyScanner.java
@@ -11,48 +11,48 @@ import java.util.Scanner;
 public class MyScanner {
     public static String getStringInput(String message) {
         try {
-            System.out.print(message);
+            System.out.print(message+": ");
             return new Scanner(System.in).next();
         } catch (InputMismatchException e) {
-            return getStringInput("Wrong input please try again: ");
+            return getStringInput("Wrong input please try again");
         }
     }
 
     public static Integer getIntInput(String message) {
         try {
-            System.out.print(message);
+            System.out.print(message+": ");
             return new Scanner(System.in).nextInt();
         } catch (InputMismatchException e) {
-            return getIntInput("Wrong input please try again: ");
+            return getIntInput("Wrong input please try again");
         }
     }
 
     public static Double getDoubleInput(String message) {
         try {
-            System.out.print(message);
+            System.out.print(message+": ");
             return new Scanner(System.in).nextDouble();
         } catch (InputMismatchException e) {
-            return getDoubleInput("Wrong input please try again: ");
+            return getDoubleInput("Wrong input please try again");
         }
     }
 
     public static Category getCategory(String message) {
         try {
-            System.out.print(message);
+            System.out.print(message+": ");
             return Category.valueOf(new Scanner(System.in).next());
         } catch (InputMismatchException e) {
-            return getCategory("Wrong input please try again: ");
+            return getCategory("Wrong input please try again");
         } catch (IllegalArgumentException e) {
-            System.out.print("Category not found, The available categories are: ");
-            printCategories();
-            return getCategory("Please enter a valid category name: ");
+            System.out.print("Category not found, The available categories are:\n ");
+            printAvailableCategories();
+            return getCategory("Please enter a valid category name");
         }
     }
 
-    private static void printCategories() {
+    private static void printAvailableCategories() {
         for (Category category : Category.values()
         ) {
-            System.out.println(category);
+            System.out.println(category.name());
         }
     }
 }
