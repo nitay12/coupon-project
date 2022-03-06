466498ae7e10cbb25fd892d58c723e6e8c375676
Nitay Caspi <NitayCaspi12@gmail.com>
2022-03-06 10:30:56 +0200

    fix bug - getting category name

--- src/com/nitay/couponproject/dal/CouponsDBDAO.java
+++ src/com/nitay/couponproject/dal/CouponsDBDAO.java
@@ -32,7 +32,7 @@ public class CouponsDBDAO implements CouponsDAO {
             String sqlStatement = "INSERT INTO coupons (company_id, category, title, description, start_date, end_date, amount, price, image) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)";
             PreparedStatement prep = connection.prepareStatement(sqlStatement, Statement.RETURN_GENERATED_KEYS);
             prep.setLong(1, coupon.getCompanyID());
-            prep.setString(2, coupon.getCategory().toString());
+            prep.setString(2, coupon.getCategory().name());
             prep.setString(3, coupon.getTitle());
             prep.setString(4, coupon.getDescription());
             prep.setDate(5, coupon.getStartDate());
@@ -59,7 +59,7 @@ public class CouponsDBDAO implements CouponsDAO {
             String sqlStatement = "UPDATE coupons SET company_id = ?, category =?, title = ?, description = ?, start_date = ?, end_date = ?, amount = ?, price = ?, image = ? WHERE id = ? ";
             PreparedStatement prep = connection.prepareStatement(sqlStatement, Statement.RETURN_GENERATED_KEYS);
             prep.setLong(1, coupon.getCompanyID());
-            prep.setString(2, coupon.getCategory().toString());
+            prep.setString(2, coupon.getCategory().name());
             prep.setString(3, coupon.getTitle());
             prep.setString(4, coupon.getDescription());
             prep.setDate(5, coupon.getStartDate());
@@ -67,12 +67,12 @@ public class CouponsDBDAO implements CouponsDAO {
             prep.setInt(7, coupon.getAmount());
             prep.setDouble(8, coupon.getPrice());
             prep.setString(9, coupon.getImage());
-            prep.setLong(10,coupon.getId());
+            prep.setLong(10, coupon.getId());
             prep.executeUpdate();
 
         } catch (SQLException e) {
             e.printStackTrace();
-            throw new CrudException(EntityType.COUPON,CrudType.UPDATE);
+            throw new CrudException(EntityType.COUPON, CrudType.UPDATE);
         }
     }
 
@@ -97,7 +97,7 @@ public class CouponsDBDAO implements CouponsDAO {
             prep.setInt(1, companyID);
         } catch (SQLException e) {
             e.printStackTrace();
-            throw new CrudException("Failed to delete all coupons of company with id: "+companyID);
+            throw new CrudException("Failed to delete all coupons of company with id: " + companyID);
         }
     }
 
@@ -159,7 +159,7 @@ public class CouponsDBDAO implements CouponsDAO {
             String sqlStatement = "SELECT * FROM coupons WHERE company_id = ? AND category = ?";
             PreparedStatement prep = connection.prepareStatement(sqlStatement);
             prep.setLong(1, companyId);
-            prep.setString(2, String.valueOf(category));
+            prep.setString(2, category.name());
             ResultSet result = prep.executeQuery();
             ArrayList<Coupon> coupons = new ArrayList<>();
             while (result.next()) {
@@ -216,7 +216,7 @@ public class CouponsDBDAO implements CouponsDAO {
             String sqlStatement = "SELECT * FROM coupons JOIN customer_coupon ON customer_id = ? AND coupon_id = id AND category = ?";
             PreparedStatement prep = connection.prepareStatement(sqlStatement);
             prep.setLong(1, customerId);
-            prep.setString(2, String.valueOf(category));
+            prep.setString(2, category.name());
             ResultSet result = prep.executeQuery();
             ArrayList<Coupon> coupons = new ArrayList<>();
             while (result.next()) {
