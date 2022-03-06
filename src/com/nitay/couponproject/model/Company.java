e649d598de2695952d9d56ec903b9bcf692be09f
Nitay Caspi <NitayCaspi12@gmail.com>
2022-03-06 10:15:35 +0200

    direct imports

--- src/com/nitay/couponproject/model/Company.java
+++ src/com/nitay/couponproject/model/Company.java
@@ -1,6 +1,9 @@
 package com.nitay.couponproject.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.ArrayList;
 
